e187b3aff849e30d3a55888f40d01d0e9fb17029
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2023-02-09 16:39:01 +0545

    added section component for different type of CV categories

---
+++ src/routes/cv/CVTypeThree.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import type { CVTypeThree } from '$lib/types';
+
+  export let type: 'Publications';
+  export let items: CVTypeThree[] = [];
+</script>
+
+<div class="pb-4">
+  <div class="grid lg:grid-cols-12 lg:gap-x-8">
+    <div class="col-span-4 text-right" />
+    <div class="col-span-8">
+      <div class="mb-4 inline-block border border-white px-2 pb-1 text-lg sm:text-xl">{type}</div>
+    </div>
+  </div>
+
+  <!-- Template for Desktop devices -->
+  <div class="hidden lg:block">
+    {#each items as item}
+      <div class="mb-4">
+        <div class="grid grid-cols-12 gap-x-8">
+          <div class="col-span-4 text-right" />
+          <div class="col-span-8">
+            <div class="font-extralight">
+              {item.description}
+            </div>
+          </div>
+        </div>
+      </div>
+    {/each}
+  </div>
+
+  <!-- Template for Mobile devices -->
+  <div class="lg:hidden">
+    {#each items as item}
+      <div class="mb-4">
+        <div class="font-extralight">
+          {item.description}
+        </div>
+      </div>
+    {/each}
+  </div>
+</div>
---
+++ src/routes/cv/CVTypeTwo.svelte
@@ -0,0 +1,58 @@
+<script lang="ts">
+  import type { CVTypeTwo } from '$lib/types';
+
+  export let type: 'Conferences';
+  export let items: CVTypeTwo[] = [];
+</script>
+
+<div class="pb-4">
+  <div class="grid lg:grid-cols-12 lg:gap-x-8">
+    <div class="col-span-4 text-right" />
+    <div class="col-span-8">
+      <div class="mb-4 inline-block border border-white px-2 pb-1 text-lg sm:text-xl">{type}</div>
+    </div>
+  </div>
+
+  <!-- Template for Desktop devices -->
+  <div class="hidden lg:block">
+    {#each items as item}
+      <div class="mb-4">
+        <div class="grid grid-flow-col grid-cols-12 gap-x-8">
+          <div class="col-span-4 text-right">
+            <div class="text-xl font-light">{item.location}</div>
+          </div>
+          <div class="col-span-8">
+            <div class="text-xl">{item.title}</div>
+          </div>
+        </div>
+        <div class="grid grid-cols-12 gap-x-8">
+          <div class="col-span-4 text-right">
+            <div class="text-lg font-extralight">{item.date}</div>
+          </div>
+          <div class="col-span-8">
+            <div class="font-extralight">{item.description}</div>
+          </div>
+        </div>
+      </div>
+    {/each}
+  </div>
+
+  <!-- Template for Mobile devices -->
+  <div class="lg:hidden">
+    {#each items as item}
+      <div class="mb-4">
+        <div class="flex justify-between">
+          <div class="text-lg">{item.title}</div>
+          <div class="text-right text-lg font-light">{item.location}</div>
+        </div>
+        <div class="mb-2 flex justify-between">
+          <div class="font-extralight" />
+          <div class="text-right font-extralight">{item.date}</div>
+        </div>
+        <div class="text-sm font-extralight">
+          {item.description}
+        </div>
+      </div>
+    {/each}
+  </div>
+</div>
