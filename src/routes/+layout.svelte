57f5a26e184aed0cfd59113c56c81de03d533fb2
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2023-02-09 19:13:23 +0545

    added page transitions

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,13 @@
   import '../app.css';
   import homepage_video from '$lib/assets/videos/brickup-inside.mp4';
   import cvpage_video from '$lib/assets/videos/dome3.mp4';
-  import { page } from '$app/stores';
+  import type { LayoutData } from './$types';
+  import { fade } from 'svelte/transition';
+
+  export let data: LayoutData;
 
   $: option =
-    $page.url.pathname === '/'
+    data.currentPath === '/'
       ? { src: homepage_video, class: 'opacity-50' }
       : { src: cvpage_video, class: 'opacity-25' };
 </script>
@@ -14,18 +17,20 @@
   <title>Mete Kutlu</title>
 </svelte:head>
 
-<div class="h-screen">
-  <video
-    autoplay
-    loop
-    muted
-    playsinline
-    src={option.src}
-    class="pointer-events-none fixed inset-0 h-full w-full object-cover {option.class}"
-  >
-    <track kind="captions" />
-  </video>
-  <div class="relative z-10 h-screen">
-    <slot />
+{#key data.currentPath}
+  <div class="h-screen" in:fade={{ duration: 200 }} out:fade={{ duration: 100 }}>
+    <video
+      autoplay
+      loop
+      muted
+      playsinline
+      src={option.src}
+      class="pointer-events-none fixed inset-0 h-full w-full object-cover {option.class}"
+    >
+      <track kind="captions" />
+    </video>
+    <div class="relative z-10 h-screen">
+      <slot />
+    </div>
   </div>
-</div>
+{/key}
