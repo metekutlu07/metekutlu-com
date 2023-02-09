aa7b093fa5f85f9471d72a656fec7d6c48cf0120
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2023-02-09 12:03:55 +0545

    updated home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,56 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex h-full w-full flex-col">
+  <div
+    class="flex grow flex-col items-center justify-center gap-12 text-center md:gap-20 xl:gap-24"
+  >
+    <section class="flex flex-col gap-2">
+      <h2 class="mb-4 text-6xl sm:text-7xl">Mete Kutlu</h2>
+
+      <div class="text-xl font-light sm:text-2xl">
+        <p>Digital scenographer</p>
+        <p>Architect</p>
+        <p>Lecturer @ Paris-Belleville</p>
+        <p>Ph.D. candidate</p>
+      </div>
+    </section>
+
+    <section class="flex flex-col gap-8">
+      <a href="https://dreampixels.fr/" target="_blank" rel="noreferrer" class="link">
+        Dream Pixels
+      </a>
+      <a href="https://spoliabotanique.com/" target="_blank" rel="noreferrer" class="link">
+        Spolia botanique
+      </a>
+      <a href="/cv" class="link">CV</a>
+    </section>
+  </div>
+
+  <footer class="flex h-44 w-full items-end justify-between p-4 text-xl font-light md:p-8">
+    <div class="flex flex-col gap-y-5">
+      <div class="flex flex-col gap-y-2">
+        <p>Join me:</p>
+        <p>
+          <a href="https://www.instagram.com/metekutlu/" target="_blank" rel="noreferrer">
+            @metekutlu
+          </a>
+        </p>
+        <p>
+          <a href="https://www.instagram.com/dreampixels.fr/" target="_blank" rel="noreferrer">
+            @dreampixels.fr
+          </a>
+        </p>
+      </div>
+      <p>
+        <a href="mailto:metekutlu@gmail.com">metekutlu@gmail.com</a>
+      </p>
+    </div>
+    <div>
+      <p>Paris, 2023</p>
+    </div>
+  </footer>
+</div>
+
+<style lang="postcss">
+  .link {
+    @apply inline-block border border-white px-6 pt-1 pb-3 text-3xl hover:bg-white hover:bg-opacity-25 sm:text-4xl;
+  }
+</style>
