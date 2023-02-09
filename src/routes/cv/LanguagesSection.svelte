f172674c790e19d8491c7b224a12207c5f688183
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2023-02-09 16:39:20 +0545

    added language section component

---
+++ src/routes/cv/LanguagesSection.svelte
@@ -0,0 +1,28 @@
+<script>
+  import { LANGUAGES } from '$lib/data/languages';
+</script>
+
+<div class="pb-4">
+  <div class="grid lg:grid-cols-12 lg:gap-x-8">
+    <div class="col-span-4 text-right" />
+    <div class="col-span-8">
+      <div class="mb-4 inline-block border border-white px-2 pb-1 text-lg sm:text-xl">
+        Languages
+      </div>
+    </div>
+  </div>
+
+  <div class="grid lg:grid-cols-12 lg:gap-x-8">
+    <div class="col-span-4 text-right" />
+    <div class="col-span-8">
+      <div class="grid grid-cols-2 md:w-2/3">
+        {#each LANGUAGES as item}
+          <div>
+            {item.language}:
+            <span class="font-extralight">{item.proficiency}</span>
+          </div>
+        {/each}
+      </div>
+    </div>
+  </div>
+</div>
