3be0731bd3f5b2ea8c397f90ff49c352b687a967
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2023-02-09 16:40:18 +0545

    added back to home button

---
+++ src/routes/cv/GoHome.svelte
@@ -0,0 +1,15 @@
+<a
+  href="/"
+  class="absolute right-4 top-4 flex h-8 w-8 items-center justify-center border border-white hover:bg-white hover:bg-opacity-25 md:right-6 lg:fixed lg:left-8 lg:top-8"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="2"
+    stroke="currentColor"
+    class="h-6 w-6"
+  >
+    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
+  </svg>
+</a>
