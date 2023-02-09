dcde516136c6e2872cbd8f48a02e8389ee00bf04
Pradip Khadka <20035221+coderpradp@users.noreply.github.com>
2023-02-09 16:37:14 +0545

    added types for cv categories

---
+++ src/lib/types.ts
@@ -0,0 +1,18 @@
+export type CVTypeOne = {
+  title: string;
+  subtitle: string;
+  location: string;
+  date: string;
+  description: string;
+};
+
+export type CVTypeTwo = {
+  title: string;
+  location: string;
+  date: string;
+  description: string;
+};
+
+export type CVTypeThree = {
+  description: string;
+};
